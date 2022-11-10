4b74cfddb07dbccd65d90a3d287ba6a35c3919db
Elcid-Chawa <elcidchawa@gmail.com>
2022-11-10 17:51:48 +0200

    modified: changed main server file name from index.js to server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express") ;
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./api-docs/swagger.json');
+
+const spenddingRouter = require("./spending");
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello Fintech-team9')
+})
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use("/spending", spenddingRouter)
+
+app.listen(port, () => {
+    console.log(`App Listening on port ${port}`)
+})
