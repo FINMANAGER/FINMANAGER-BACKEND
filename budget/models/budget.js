6e9517bce62b72f96dc4dfa82408a9097723e48e
aronmakiika <aronmakiika@gmail.com>
2022-11-13 15:32:34 +0300

    fix: delete budget repo

--- budget/models/budget.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose')
-const Schema = mongoose.Schema;
-
-const budgetSchema = new Schema({
-    budget: Number,
-    date: Date 
-});
-
-module.exports= mongoose.model('budget', budgetSchema);
