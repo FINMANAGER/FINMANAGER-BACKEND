c864a2332bcdfea3560a8647a05da6bf7dd25016
aronmakiika <aronmakiika@gmail.com>
2022-11-19 22:06:37 +0300

    feat: add user validation

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -2,5 +2,9 @@ import yup from "yup";
 
 export const userModel = yup.object({
   username: yup.string().required().email(),
-  password: yup.string().required().min(4).max(15),
+  password: yup
+    .string()
+    .required("you need to provide the password")
+    .min(4)
+    .max(15),
 });
