10105865a17f01a82ffbfa5a868d4ed38ef2c62a
Elcid-Chawa <elcidchawa@gmail.com>
2022-11-21 06:51:46 +0200

    update: added configuration
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
-const firebase = require("firebase-admin");
-const config = require("./config");
+const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { auth } = require('firebase-admin')
+const serviceAccount = require("./serviceAccount.json")
+const {firebaseConfig} = require('./config')
 
-const db = firebase.initializeApp(config.firebaseConfig)
+const app = initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: "https://finmanager-f2308.firebaseio.com"
+});
 
-module.exports = db;
+const db = getFirestore(app);
+
+module.exports = {db, auth, app};
