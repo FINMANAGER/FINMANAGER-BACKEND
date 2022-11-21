3d876314a65ac6ec22eddad75cadb5829d41b98f
Elcid-Chawa <elcidchawa@gmail.com>
2022-11-21 06:54:32 +0200

    add: added profile code
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

--- profile/models/profile.js
+++ profile/models/profile.js
@@ -1,3 +1,11 @@
 class Profile {
-    constructor(id, user, spending, budget)
+    constructor(id, user_id, spending, budget, categories, username, fullnames){
+        this.id = id
+        this.user_id = user_id
+        this.spending = spending
+        this.budget = budget
+        this.categories = categories
+        this.username
+        fullnames
+    }
 }
