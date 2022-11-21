274fe02259d4370ec118aa76398610e6428b1a8c
Elcid-Chawa <elcidchawa@gmail.com>
2022-11-21 06:51:30 +0200

    update: added configuration
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

--- config.js
+++ config.js
@@ -22,7 +22,6 @@ module.exports = {
     firebaseConfig: {
         apiKey: API_KEY,
         authDomain: AUTH_DOMAIN,
-        databaseURL: DATABASE_URL,
         projectId: PROJECT_ID,
         storageBucket: STORAGE_BUCKET,
         messagingSenderId: MESSAGING_SENDER_ID,
