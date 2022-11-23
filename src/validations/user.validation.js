a4af8871101c29a84c5a6fee94544bbd4062b1d5
Elcid-Chawa <elcidchawa@gmail.com>
2022-11-23 11:19:06 +0100

    update: added spending validation model
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -8,3 +8,9 @@ export const userModel = yup.object({
     .min(4)
     .max(15),
 });
+
+export const spendingModel = yup.object({
+  amount: yup.number().required("Need to enter  bending amount"),
+  date: yup.date(),
+  category: yup.string().required("select a category")
+})
