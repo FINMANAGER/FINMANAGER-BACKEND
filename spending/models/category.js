4183ea0d60a58c9afe6a23b33069b5f5f1a79997
Elcid-Chawa <elcidchawa@gmail.com>
2022-11-29 08:22:29 +0100

    delete: removed unsed files
    Signed-off-by: Elcid-Chawa <elcidchawa@gmail.com>

--- spending/models/category.js
+++
@@ -1,7 +0,0 @@
-class Category {
-    constructor (id, name) {
-        this.id = id
-    }
-}
-
-module.exports = Category;
--- users/routes/auth.router.js
+++
@@ -1,8 +0,0 @@
-const express =  require("express");
-const authRouter = express.Router();
-const {signUp, signIn} = require("../../auth")
-
-authRouter.post("/signup", signUp);
-authRouter.post("/signIn", signIn);
-
-module.exports = authRouter;
